c8538a2bb0495481b837ed7b940035c603f4df89
Hasan <h.mjabai92@gmail.com>
2023-12-23 18:11:03 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here
+class Dog {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+}
+
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class lunch {
+  #dessert;
+  constructor(salad, soup) {
+    this.salad = salad;
+    this.soup = soup;
+  }
+}
+
+class dinner {
+  #dessert;
+  constructor(salad, soup, entree) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+  }
+}
